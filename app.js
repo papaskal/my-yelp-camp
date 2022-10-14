4612ffa3a77689c465010699912d194167d0f1c7
Alexandros Papaskarlatos <alexpapask@gmail.com>
2022-10-14 22:05:09 +0300

    Added basic styles, boilerplate, navbar, footer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 
 const Campground = require('./models/campground')
@@ -10,51 +11,52 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true,
     useUnifiedTopology: true
-});
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error:"));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection error:"))
 db.once("open", () => {
-    console.log("Database connected");
-});
+    console.log("Database connected")
+})
 
 
-const app = express();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const app = express()
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000');
-});
+    console.log('Serving on port 3000')
+})
 
 app.get('/', (req, res) => {
     // res.send('Hello from Yelp Camp')
-    res.render('home');
-});
+    res.render('home')
+})
 
 app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({})
     // res.send(campgrounds.map(x => x.title))
-    res.render('campgrounds/index', { campgrounds });
-});
+    res.render('campgrounds/index', { campgrounds })
+})
 
 app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
+    res.render('campgrounds/new')
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground });
-});
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
+})
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground });
-
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
 })
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const { readdirSync } = require('fs');
+const path = require('path');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -17,15 +19,20 @@ db.once("open", () => {
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
-
+// const imageList = readdirSync(path.join(__dirname, 'images')).map(x => path.join(__dirname, x));
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            // image: `${sample(imageList)}`,
+            image: "https://images.unsplash.com/photo-1501707305551-9b2adda5e527?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80",
+            description: 'lorem asdas das diuhnflskduhifjn oilh ighloi l iuyasihd nbuyiguh jao hindiuh ajskn iuoh ',
+            price
         })
         await camp.save();
     }
